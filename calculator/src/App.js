cd0ad24f086a8a1af753c18ea4766d005ef6a6a8
Yash <yashuan0202@gmail.com>
2024-11-10 00:44:56 +0530

    app.js

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -8,32 +8,42 @@ function App() {
   const inputRef = useRef(null); 
   const resultRef = useRef(null); 
   const [result, setResult] = useState(0); 
- 
+
   function plus(e) { 
     e.preventDefault(); 
     setResult((result) => result + Number(inputRef.current.value)); 
-  }; 
- 
+  }
+
   function minus(e) { 
-  	// Add the code for the minus function 
+    e.preventDefault();
+    setResult((result) => result - Number(inputRef.current.value));
   };
- 
+
   function times(e) { 
-    // Add the code for the plus function 
-  }; 
- 
-  function divide(e) { 
-    // Add the code for the divide function 
+    e.preventDefault();
+    setResult((result) => result * Number(inputRef.current.value));
   };
- 
+
+  function divide(e) { 
+    e.preventDefault();
+    const inputValue = Number(inputRef.current.value);
+    if (inputValue !== 0) {
+      setResult((result) => result / inputValue);
+    } else {
+      alert("Cannot divide by zero");
+    }
+  };
+
   function resetInput(e) { 
-    // Add the code for the resetInput function 
-  }; 
- 
+    e.preventDefault();
+    inputRef.current.value = "";
+  };
+
   function resetResult(e) { 
-  	// Add the code for the resetResult function 
-  }; 
- 
+    e.preventDefault();
+    setResult(0);
+  };
+
   return ( 
     <div className="App"> 
       <div> 
@@ -41,23 +51,22 @@ function App() {
       </div> 
       <form> 
         <p ref={resultRef}> 
-          {/* add the value of the current total */} 
+          Result: {result}
         </p> 
         <input
-          pattern="[0-9]" 
           ref={inputRef} 
           type="number" 
           placeholder="Type a number" 
         /> 
-        <button onClick={plus}>add</button> 
-        {/* Add the subtract button */} 
-        {/* Add the multiply button */} 
-        {/* Add the divide button */} 
-        {/* Add the resetInput button */} 
-        {/* Add the resetResult button */} 
+        <button onClick={plus}>Add</button> 
+        <button onClick={minus}>Subtract</button> 
+        <button onClick={times}>Multiply</button> 
+        <button onClick={divide}>Divide</button> 
+        <button onClick={resetInput}>Reset Input</button> 
+        <button onClick={resetResult}>Reset Result</button> 
       </form> 
     </div> 
   ); 
-} 
- 
-export default App; 
+}
+
+export default App;
